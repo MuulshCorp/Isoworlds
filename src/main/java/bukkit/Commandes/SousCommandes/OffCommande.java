3d644e5586f69335cfc6de788d171a36cdaab7a2
isolonice <isolonice@gmail.com>
2017-11-20 13:36:25 +0100

    Bukkit: Kick players on off world

--- src/main/java/bukkit/Commandes/SousCommandes/OffCommande.java
+++ src/main/java/bukkit/Commandes/SousCommandes/OffCommande.java
@@ -1,6 +1,7 @@
 package bukkit.Commandes.SousCommandes;
 
 import bukkit.IsoworldsBukkit;
+import bukkit.Locations.IsoworldsLocations;
 import bukkit.Utils.IsoworldsUtils;
 import common.Msg;
 import org.bukkit.Bukkit;
@@ -45,6 +46,10 @@ public class OffCommande {
 
         // Si le monde n'est pas chargé alors on le fait et on le save avant
         if (Bukkit.getServer().getWorld(worldname) != null) {
+            // Kick des joueurs
+            for (Player p : Bukkit.getServer().getWorld(worldname).getPlayers()) {
+                IsoworldsLocations.teleport(p, "Isolonice");
+            }
             Bukkit.getServer().unloadWorld(worldname, true);
             pPlayer.sendMessage(ChatColor.GOLD + "[IsoWorlds]: Sijania vient de désactiver votre IsoWorld." + ChatColor.AQUA + Msg.keys.EXISTE_PAS_IWORLD);
         }

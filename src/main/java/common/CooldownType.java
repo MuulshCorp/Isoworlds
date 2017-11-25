6f255e460ab424592ddb531632bb7857d072ee1d
François Mathieu <maps_red@yahoo.fr>
2017-11-25 18:28:15 +0100

    Update Cooldown class
    (cherry picked from commit eecc766)
    
    (cherry picked from commit ae46c40)

---
+++ src/main/java/common/CooldownType.java
@@ -0,0 +1,39 @@
+package common;
+
+public interface CooldownType {
+    String BIOMECOMMANDE = "biome";
+    int BIOMECOMMANDE_DELAY = 86400;
+
+    String CONFIANCE = "confiance";
+    int CONFIANCE_DELAY = 86400;
+
+    String CREATION = "creation";
+    int CREATION_DELAY = 86400;
+
+    String LISTE = "liste";
+    int LISTE_DELAY = 86400;
+
+    String MAISON = "maison";
+    int MAISON_DELAY = 86400;
+
+    String METEO = "meteo";
+    int METEO_DELAY = 86400;
+
+    String OFF = "off";
+    int OFF_DELAY = 86400;
+
+    String ON = "on";
+    int ON_DELAY = 86400;
+
+    String REFONTE = "refonte";
+    int REFONTE_DELAY = 86400;
+
+    String RETIRERCONFIANCE = "retirerconfiance";
+    int RETIRERCONFIANCE_DELAY = 86400;
+
+    String TELEPORT = "teleport";
+    int TELEPORT_DELAY = 86400;
+
+    String TIME = "time";
+    int TIME_DELAY = 86400;
+}

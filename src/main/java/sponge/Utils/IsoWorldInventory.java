b915fc19d8511b60cd022b331fced3c42a26054f
Isolonice <isolonice@gmail.com>
2017-11-26 05:42:22 +0100

    - Sponge: fixed meteo & time world name

--- src/main/java/sponge/Utils/IsoWorldInventory.java
+++ src/main/java/sponge/Utils/IsoWorldInventory.java
@@ -317,21 +317,21 @@ public class IsoWorldInventory {
                     if (menuName.contains("Soleil")) {
                         mtype = "soleil";
                     } else if (menuName.contains("Pluie")) {
-                        mtype = "soleil";
+                        mtype = "pluie";
                     } else if (menuName.contains("Orage")) {
-                        mtype = "soleil";
+                        mtype = "orage";
                     } else if (menuName.contains("Menu principal")) {
                         closeOpenMenu(pPlayer, menuPrincipal(pPlayer));
                     }
 
                     if (menuName.contains("10 minutes")) {
-                        commandMenu(pPlayer, "iw meteo " + mtype + "12000");
+                        commandMenu(pPlayer, "iw meteo " + mtype + " 12000 " + pPlayer.getUniqueId().toString() + "-IsoWorld");
                         closeMenu(pPlayer);
                     } else if (menuName.contains("30 minutes")) {
-                        commandMenu(pPlayer, "iw meteo " + mtype + "36000");
+                        commandMenu(pPlayer, "iw meteo " + mtype + " 36000 " + pPlayer.getUniqueId().toString() + "-IsoWorld");
                         closeMenu(pPlayer);
                     } else if (menuName.contains("1 heure")) {
-                        commandMenu(pPlayer, "iw meteo " + mtype + "72000");
+                        commandMenu(pPlayer, "iw meteo " + mtype + " 72000 " + pPlayer.getUniqueId().toString() + "-IsoWorld");
                         closeMenu(pPlayer);
                     }
 
@@ -492,10 +492,10 @@ public class IsoWorldInventory {
                             .get(0).getOriginal().get(Keys.DISPLAY_NAME).get().toPlain());
                     clickInventoryEvent.setCancelled(true);
                     if (menuName.contains("Jour")) {
-                        Sponge.getCommandManager().process(pPlayer, "iw temps jour");
+                        Sponge.getCommandManager().process(pPlayer, "iw temps jour " + pPlayer.getUniqueId().toString() + "-IsoWorld");
                         pPlayer.closeInventory();
                     } else if (menuName.contains("Nuit")) {
-                        Sponge.getCommandManager().process(pPlayer, "iw temps nuit");
+                        Sponge.getCommandManager().process(pPlayer, "iw temps nuit " + pPlayer.getUniqueId().toString() + "-IsoWorld");
                         pPlayer.closeInventory();
                     }
                 })

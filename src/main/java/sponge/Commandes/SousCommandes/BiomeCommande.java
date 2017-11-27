f399b663ad7f999e9ad33a31db636da8e2a71ddc
Isolonice <isolonice@gmail.com>
2017-11-27 01:47:24 +0100

    - Sponge: Fixing call remove confiance gui - Sponge: Making biome cmd content - Sponge: Added skin to menu confiance

--- src/main/java/sponge/Commandes/SousCommandes/BiomeCommande.java
+++ src/main/java/sponge/Commandes/SousCommandes/BiomeCommande.java
@@ -1,34 +1,98 @@
 package sponge.Commandes.SousCommandes;
 
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandCallable;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.Chunk;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.biome.BiomeType;
+import org.spongepowered.api.world.biome.BiomeTypes;
 import sponge.IsoworldsSponge;
+import sponge.Utils.IsoworldsUtils;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Optional;
 
 import static sponge.IsoworldsSponge.instance;
+import static sponge.Utils.IsoworldsUtils.isLocked;
 
 /**
  * Created by Edwin on 20/11/2017.
  */
-public class BiomeCommande implements CommandExecutor {
 
-    private final IsoworldsSponge plugin = instance;
+public class BiomeCommande implements CommandCallable {
+
+    private final IsoworldsSponge plugin = IsoworldsSponge.instance;
 
     @Override
-    public CommandResult execute(CommandSource source, CommandContext args) throws CommandException {
+    public CommandResult process(CommandSource source, String args) throws CommandException {
+        Player pPlayer = (Player) source;
+        World world = pPlayer.getWorld();
+        String[] arg = args.split(" ");
+        int size = arg.length;
+        BiomeType biome;
+
+        // Si la méthode renvoi vrai alors on return car le lock est défini, sinon elle le set auto
+        if (isLocked(pPlayer, String.class.getName())) {
+            return CommandResult.success();
+        }
+
+        if (arg[0] == "plaines") {
+            biome = BiomeTypes.PLAINS;
+        } else if (arg[0] == "desert") {
+            biome = BiomeTypes.DESERT;
+        } else {
+            instance.lock.remove(pPlayer.getUniqueId().toString() + ";" + String.class.getName());
+            return CommandResult.success();
+        }
+        //Get chunk
+        int ChunkX = pPlayer.getLocation().getBlockPosition().getX();
+        int ChunkY = pPlayer.getLocation().getBlockPosition().getY();
+        // Définition biome
+        Optional<Chunk> chunk = world.getChunk(ChunkX, 0, ChunkY);
+        for (int x = 0; x < 16; x++) {
+            for (int z = 0; z < 16; z++) {
+                chunk.get().setBiome(x, 0, z, biome);
+            }
+        }
+        //pPlayer.sendMessage("[IsoWorlds]: " + "Sijania vient de changer le biome du chunk dans lequel vous êtes. (F9)");
+        instance.lock.remove(pPlayer.getUniqueId().toString() + ";" + String.class.getName());
         return CommandResult.success();
     }
 
-    // Constructeurs
-    public static CommandSpec getCommand() {
-        return CommandSpec.builder()
-                .description(Text.of("Commande permettant de changer de biome dans son IsoWorld."))
-                .permission("isoworlds.biome")
-                .executor(new BiomeCommande())
-                .build();
+
+    @Override
+    public List<String> getSuggestions(CommandSource source, String arguments, @Nullable Location<World> targetPosition) throws CommandException {
+        return null;
     }
+
+    @Override
+    public boolean testPermission(CommandSource source) {
+        return false;
+    }
+
+    @Override
+    public Optional<Text> getShortDescription(CommandSource source) {
+        return null;
+    }
+
+    @Override
+    public Optional<Text> getHelp(CommandSource source) {
+        return null;
+    }
+
+    @Override
+    public Text getUsage(CommandSource source) {
+        return null;
+    }
+
 }
--- src/main/java/sponge/Utils/IsoWorldInventory.java
+++ src/main/java/sponge/Utils/IsoWorldInventory.java
@@ -4,6 +4,9 @@ import common.Msg;
 import javafx.scene.control.cell.TextFieldListCell;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.mutable.RepresentedPlayerData;
+import org.spongepowered.api.data.manipulator.mutable.SkullData;
+import org.spongepowered.api.data.type.SkullTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.item.inventory.ClickInventoryEvent;
 import org.spongepowered.api.item.ItemTypes;
@@ -13,6 +16,7 @@ import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.property.InventoryDimension;
 import org.spongepowered.api.item.inventory.property.InventoryTitle;
 import org.spongepowered.api.item.inventory.property.SlotPos;
+import org.spongepowered.api.profile.GameProfile;
 import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
@@ -229,12 +233,14 @@ public class IsoWorldInventory {
                 .listener(ClickInventoryEvent.class, clickInventoryEvent -> {
                     // Code event
                     String menuName = String.valueOf(clickInventoryEvent.getTransactions()
+                            .get(0).getOriginal().get(Keys.ITEM_LORE).get().toString());
+                    String menuPlayer = String.valueOf(clickInventoryEvent.getTransactions()
                             .get(0).getOriginal().get(Keys.DISPLAY_NAME).get().toPlain());
                     IsoworldsUtils.cm("CURSOR 2 " + String.valueOf(clickInventoryEvent.getTransactions().get(0).getOriginal().get(Keys.DISPLAY_NAME).get().toPlain()));
                     clickInventoryEvent.setCancelled(true);
                     // Si joueur, on ajouter le joueur
                     if (menuName.contains("Joueur")) {
-                        commandMenu(pPlayer, "iw confiance " + menuName);
+                        commandMenu(pPlayer, "iw confiance " + menuPlayer);
                         closeOpenMenu(pPlayer, menuPrincipal(pPlayer));
                     } else if (menuName.contains("Menu principal")) {
                         closeOpenMenu(pPlayer, menuPrincipal(pPlayer));
@@ -249,20 +255,29 @@ public class IsoWorldInventory {
         for (Player p : Sponge.getServer().getOnlinePlayers()) {
             if (!IsoworldsUtils.trustExists(p, pPlayer.getUniqueId(), Msg.keys.EXISTE_PAS_TRUST)) {
                 List<Text> list1 = new ArrayList<Text>();
-                list1.add(Text.of("Joueur: " + p.getName()));
-                ItemStack item1 = ItemStack.builder().itemType(ItemTypes.SKULL).add(Keys.ITEM_LORE, list1).add(Keys.DISPLAY_NAME, Text.of(Text.builder("Cliquer pour ajouter")
-                        .color(TextColors.GOLD).build())).quantity(1).build();
+                list1.add(Text.of("Joueur"));
+
+                SkullData data = Sponge.getGame().getDataManager().getManipulatorBuilder(SkullData.class).get().create();
+                data.set(Keys.SKULL_TYPE, SkullTypes.PLAYER);
+                ItemStack stack = Sponge.getGame().getRegistry().createBuilder(ItemStack.Builder.class).itemType(ItemTypes.SKULL).itemData(data)
+                        .add(Keys.ITEM_LORE, list1).add(Keys.DISPLAY_NAME, Text.of(Text.builder(p.getName())
+                                .color(TextColors.GOLD).build())).quantity(1)
+                        .build();
+                RepresentedPlayerData skinData = Sponge.getGame().getDataManager().getManipulatorBuilder(RepresentedPlayerData.class).get().create();
+                skinData.set(Keys.REPRESENTED_PLAYER, GameProfile.of(UUID.fromString(p.getUniqueId().toString()), p.getName()));
+                stack.offer(skinData);
+
                 if (i >= 8) {
                     j = j++;
                 }
-                menu.query(SlotPos.of(i, j)).set(item1);
+                menu.query(SlotPos.of(i, j)).set(stack);
             }
         }
 
         List<Text> list2 = new ArrayList<Text>();
         list2.add(Text.of("Menu principal"));
 
-        ItemStack item2 = ItemStack.builder().itemType(ItemTypes.GRASS).add(Keys.ITEM_LORE, list2).add(Keys.DISPLAY_NAME, Text.of(Text.builder("Retirer")
+        ItemStack item2 = ItemStack.builder().itemType(ItemTypes.GOLD_BLOCK).add(Keys.ITEM_LORE, list2  ).add(Keys.DISPLAY_NAME, Text.of(Text.builder("Menu principal")
                 .color(TextColors.RED).build())).quantity(1).build();
         menu.query(SlotPos.of(26, 3)).set(item2);
 
@@ -277,12 +292,14 @@ public class IsoWorldInventory {
                 .listener(ClickInventoryEvent.class, clickInventoryEvent -> {
                     // Code event
                     String menuName = String.valueOf(clickInventoryEvent.getTransactions()
+                            .get(0).getOriginal().get(Keys.ITEM_LORE).get().toString());
+                    String menuPlayer = String.valueOf(clickInventoryEvent.getTransactions()
                             .get(0).getOriginal().get(Keys.DISPLAY_NAME).get().toPlain());
                     IsoworldsUtils.cm("CURSOR 2 " + String.valueOf(clickInventoryEvent.getTransactions().get(0).getOriginal().get(Keys.DISPLAY_NAME).get().toPlain()));
                     clickInventoryEvent.setCancelled(true);
                     // Si joueur, on ajouter le joueur
                     if (menuName.contains("Joueur")) {
-                        commandMenu(pPlayer, "iw retirer " + menuName);
+                        commandMenu(pPlayer, "iw retirer " + menuPlayer);
                         closeOpenMenu(pPlayer, menuPrincipal(pPlayer));
                     } else if (menuName.contains("Menu principal")) {
                         closeOpenMenu(pPlayer, menuPrincipal(pPlayer));
@@ -297,20 +314,29 @@ public class IsoWorldInventory {
         for (Player p : Sponge.getServer().getOnlinePlayers()) {
             if (IsoworldsUtils.trustExists(p, pPlayer.getUniqueId(), Msg.keys.EXISTE_PAS_TRUST)) {
                 List<Text> list1 = new ArrayList<Text>();
-                list1.add(Text.of("Joueur: " + p.getName()));
-                ItemStack item1 = ItemStack.builder().itemType(ItemTypes.SKULL).add(Keys.ITEM_LORE, list1).add(Keys.DISPLAY_NAME, Text.of(Text.builder("Cliquer pour retirer")
-                        .color(TextColors.GOLD).build())).quantity(1).build();
+                list1.add(Text.of("Joueur"));
+
+                SkullData data = Sponge.getGame().getDataManager().getManipulatorBuilder(SkullData.class).get().create();
+                data.set(Keys.SKULL_TYPE, SkullTypes.PLAYER);
+                ItemStack stack = Sponge.getGame().getRegistry().createBuilder(ItemStack.Builder.class).itemType(ItemTypes.SKULL).itemData(data)
+                        .add(Keys.ITEM_LORE, list1).add(Keys.DISPLAY_NAME, Text.of(Text.builder(p.getName())
+                        .color(TextColors.GOLD).build())).quantity(1)
+                        .build();
+                RepresentedPlayerData skinData = Sponge.getGame().getDataManager().getManipulatorBuilder(RepresentedPlayerData.class).get().create();
+                skinData.set(Keys.REPRESENTED_PLAYER, GameProfile.of(UUID.fromString(p.getUniqueId().toString()), p.getName()));
+                stack.offer(skinData);
+
                 if (i >= 8) {
                     j = j++;
                 }
-                menu.query(SlotPos.of(i, j)).set(item1);
+                menu.query(SlotPos.of(i, j)).set(stack);
             }
         }
 
         List<Text> list2 = new ArrayList<Text>();
         list2.add(Text.of("Menu principal"));
 
-        ItemStack item2 = ItemStack.builder().itemType(ItemTypes.GRASS).add(Keys.ITEM_LORE, list2).add(Keys.DISPLAY_NAME, Text.of(Text.builder("Retirer")
+        ItemStack item2 = ItemStack.builder().itemType(ItemTypes.GOLD_BLOCK).add(Keys.ITEM_LORE, list2).add(Keys.DISPLAY_NAME, Text.of(Text.builder("Menu principal")
                 .color(TextColors.RED).build())).quantity(1).build();
         menu.query(SlotPos.of(26, 3)).set(item2);
 

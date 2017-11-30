bd97155896954eb0b56e642c771f1c6d14d66adc
Isolonice <isolonice@gmail.com>
2017-11-30 23:10:21 +0100

    - Sponge: ITEMAMETTRE
    (cherry picked from commit 209472d)

--- src/main/java/sponge/Utils/IsoWorldsBanWorldItems.java
+++ src/main/java/sponge/Utils/IsoWorldsBanWorldItems.java
@@ -35,7 +35,7 @@ public class IsoWorldsBanWorldItems {
                                 for (int z = min.getZ(); z <= max.getZ(); z++) {
                                     BlockState block = chunk.getBlock(x, y, z);
                                     Location blockLoc = chunk.getLocation(x, y, z);
-                                    if (block.getType().getName().contains("minecraft:wool")) {
+                                    if (block.getType().getName().contains("ITEMAMETTREICI")) {
                                         int finalX = x;
                                         int finalY = y;
                                         int finalZ = z;

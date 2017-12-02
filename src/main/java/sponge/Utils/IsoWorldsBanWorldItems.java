d211e8b01ff8cb369915b34d23315a525f353810
Isolonice <isolonice@gmail.com>
2017-12-02 11:27:34 +0100

    - Sponge: Added forestry hives on ban
    (cherry picked from commit 0434c76)

--- src/main/java/sponge/Utils/IsoWorldsBanWorldItems.java
+++ src/main/java/sponge/Utils/IsoWorldsBanWorldItems.java
@@ -35,7 +35,7 @@ public class IsoWorldsBanWorldItems {
                                 for (int z = min.getZ(); z <= max.getZ(); z++) {
                                     BlockState block = chunk.getBlock(x, y, z);
                                     Location blockLoc = chunk.getLocation(x, y, z);
-                                    if (block.getType().getName().contains("magicbees:hiveblock")) {
+                                    if (block.getType().getName().contains("magicbees:hiveblock") || block.getType().getName().contains("forestry:beehives")) {
                                         int finalX = x;
                                         int finalY = y;
                                         int finalZ = z;
